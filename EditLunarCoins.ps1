dd52824ae0b47c3410c367c2b4f4a03f7889c5d0
Yagel Y <50696200+yagely80@users.noreply.github.com>
2022-03-17 15:08:54 +0200

    initial commit

---
+++ EditLunarCoins.ps1
@@ -0,0 +1,8 @@
+﻿$userid = "enter_your_id_here"
+
+$path = "C:\\Program Files (x86)\\Steam\\userdata\\" + $userid + "\\632360\\remote\\UserProfiles"
+$filepath = $path+"\\"+(Get-ChildItem $path).Name
+
+$content = Get-Content $filepath
+$content = $content -replace "<coins>.*</coins>","<coins>1000000000</coins>"
+Out-File -FilePath $filepath -InputObject $content
